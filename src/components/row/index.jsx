000ab23200eddeab540b36990f7a90ee3f6a6682
apple <apple@appledeMacBook-Pro-6.local>
2021-05-13 15:14:39 +0800

    修改组件

--- src/components/row/index.jsx
+++ src/components/row/index.jsx
@@ -10,7 +10,6 @@ const RowFomat = () => {
     alignItems: 'center',
     justifyContent: 'space-between',
     borderBottom: '1px solid rgb(211,211,211)',
-    padding: '15px 0',
   };
 
   const toHref = () => {
@@ -18,11 +17,11 @@ const RowFomat = () => {
   };
 
   return (
-    <Row align="middle">
-      <Col span={5} >
+    <Row align="stretch" onClick={() => toHref()}>
+      <Col span={5} style={{padding: '5px 0'}}>
         <img src={icon} style={{ width: '100%', borderRadius: 15 }} />
       </Col>
-      <Col span={18} offset={1} style={style} onClick={() => toHref()}>
+      <Col span={18} offset={1} style={style}>
         <div className={styles.titles}>
           <h3>抖音能放啥感觉呢</h3>
           <h4>记录美好生活无纺布生机勃发建设步伐加快</h4>
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Title from '../../components/title';
-import Recommendation from '../../components/recommendation';
+import Row from '../../components/row';
 import styles from './index.less';
 
 const search = () => {
   const [isShown, setIsShown] = useState(false);
+  const [list, setList] = useState([]);
+
   const isHeaderShown = (value) => {
     setIsShown(value);
   };
+
+  useEffect(() => {
+    const data = [];
+    for (let i = 0; i < 16; i++) {
+      data.push(<Row />);
+    }
+    setList(data);
+  }, []);
+
   return (
     <>
       <Header title="搜索" header={isShown} />
@@ -18,13 +29,17 @@ const search = () => {
         <div className={styles.more}>
           <h2>探索更多</h2>
           <ul>
-            <li style={{}}>小游戏</li>
+            <li>小游戏</li>
             <li>网络电话</li>
             <li>rpg</li>
             <li>求职招聘</li>
           </ul>
         </div>
-        <Recommendation />
+        <div className={styles.list}>
+          {list.map((item, key) => (
+            <div key={key}>{item}</div>
+          ))}
+        </div>
       </main>
       <div style={{ height: 70, width: '100%' }} />
       <Footer />
